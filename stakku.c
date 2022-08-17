35c6cd2a2f275965231eaa0f5021d329fd6ed1dc
tibikoupen <kou-pen@outlook.jp>
2022-08-17 09:23:16 +0900

    [clean] stakku.c

--- stakku.c
+++ stakku.c
@@ -8,18 +8,17 @@
 
 #define data_size 100 /*最大の個数*/
 
-typedef struct STACK
-{
+typedef struct STACK{
     /* data */
     int final;
     int data[data_size];
 }STACK_T;
 
-void initstack(STACK_T*);
-void printstack(STACK_T*);
-void pushtostack(STACK_T*,int);
-int popfromstack(STACK_T*);
-int peekfromstack(STACK_T*);
+void init_stack(STACK_T*);
+void print_stack(STACK_T*);
+void push_to_stack(STACK_T*,int);
+int pop_from_stack(STACK_T*);
+int peek_from_stack(STACK_T*);
 
 /*ctrl+c 処理*/
 void signal_handler(int signum){
@@ -28,14 +27,13 @@ void signal_handler(int signum){
 }
 
 /*スタック初期化*/
-void initstack(STACK_T *stack){
+void init_stack(STACK_T *stack){
     stack->final = -1;
 }
 
 /*push関数*/
-void pushtostack(STACK_T *stack,int input){
-    if (stack->final >= data_size -1)
-    {
+void push_to_stack(STACK_T *stack,int input){
+    if (stack->final >= data_size -1){
         /* code */
         printf("スタックがいっぱいです！");
         return;
@@ -45,29 +43,28 @@ void pushtostack(STACK_T *stack,int input){
 }
 
 /*pop用関数*/
-int popfromstack(STACK_T *stack){
-    int getdata =0;
+int pop_from_stack(STACK_T *stack){
+    int get_data =0;
     if(stack->final == -1){
         printf("スタックがすっからかんです！\n");
         return -1;
     }
-    getdata = stack->data[stack->final];
+    get_data = stack->data[stack->final];
     stack->final = stack->final -1;
-    return getdata;
+    return get_data;
 }
 
 /*全部表示する関数。主に動作チェック用*/
-void printstack(STACK_T *stack){
+void print_stack(STACK_T *stack){
     int i =0;
     printf("すべてのデータ:\n");
-    for ( i = 0; i <= stack->final; i++)
-    {   
+    for ( i = 0; i <= stack->final; i++){   
         printf("%d\n",stack->data[stack->final -i]);
     }
 }
 
 /*peek関数*/
-int peekfromstack(STACK_T *stack){
+int peek_from_stack(STACK_T *stack){
     if(stack->final == -1){
         printf("スタックがすっからかんです！\n");
         return -1;
@@ -83,7 +80,7 @@ int main(void){
     STACK_T stack;
     signal(SIGINT, signal_handler);/*シグナルハンドラ登録*/
 
-    initstack(&stack);
+    init_stack(&stack);
     system("cls");
 
     while(1){
@@ -91,8 +88,7 @@ int main(void){
         printf("push:1,pop all:2,pop:3,peek:4,終了:ctrl+cか他のキー\n");
         scanf("%d",&ans);
         system("cls");
-        if (ans == 1)/*push*/
-        {
+        if (ans == 1)/*push*/{
             /* code */
             printf("pushしたい整数を入力");
             scanf("%d",&input);
@@ -101,28 +97,25 @@ int main(void){
                 printf("これは負の数です\n");
                 continue;
             }
-            pushtostack(&stack,input);
+            push_to_stack(&stack,input);
             printf("pushされました\n");
         }
-        else if (ans == 3)/*pop*/
-        {
-            output = popfromstack(&stack);
+        else if (ans == 3)/*pop*/{
+            output = pop_from_stack(&stack);
             if(output != -1){
                 system("cls");
                 printf("%dです\n",output);
             }
         }
-        else if (ans ==2)/*print all*/
-        {
+        else if (ans ==2)/*print all*/{
             /* code */
             system("cls");
-            printstack(&stack);
+            print_stack(&stack);
         }
-        else if (ans ==4)/*peek*/
-        {
+        else if (ans ==4)/*peek*/{
             /* code */
             system("cls");
-            peekfromstack(&stack);
+            peek_from_stack(&stack);
         }
         else{ /*else*/
             printf("プログラムを終了します");
